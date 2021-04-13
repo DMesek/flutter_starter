866ea1ead290069a0c2c56d06f16e407c995e35e
dmesek <dominik.mesek@q.agency>
2021-04-13 10:58:42 +0200

    maybe new version

--- scripts/setup_flavors.sh
+++ scripts/setup_flavors.sh
@@ -4,15 +4,15 @@ packageId="$2"
 stagingId="${packageId}.staging"
 devId="${packageId}.dev"
 
+echo "Setting up flavors..."
 cat pubspec.yaml | sed "s/\${APP_NAME}/$appName/g" | sed "s/\${ID_DEV}/$devId/g" | sed "s/\${ID_STAGING}/$stagingId/g" | sed "s/\${ID_PROD}/$packageId/g" > scripts/tmp.yaml
 rm pubspec.yaml 
 mv scripts/tmp.yaml pubspec.yaml
 
-echo "Setting up flavors..."
 flutter pub get
-flutter pub run flutter_flavorizr
-rm lib/app.dart lib/flavors.dart lib/main-dev.dart lib/main-staging.dart lib/main-prod.dart
-rm -r lib/pages
+flutter pub run flutter_flavorizr -p android:buildGradle,android:androidManifest,android:dummyAssets,google:firebase,ios:xcconfig,ios:buildTargets,ios:schema,ios:dummyAssets,ios:plist
+# rm lib/app.dart lib/flavors.dart lib/main-dev.dart lib/main-staging.dart lib/main-prod.dart
+# rm -r lib/pages
 
 cat lib/main/app_environment.dart | sed "s/APP_NAME/$appName/g" > tmp.dart
 rm lib/main/app_environment.dart && mv tmp.dart lib/main/app_environment.dart
